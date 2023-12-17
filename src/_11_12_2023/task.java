187a1c8bbc2c2e1d6d1c20caddac590cf55000b6
Savelii.Apanasenko <2005savaapanasenko@gmail.com>
2023-12-17 20:39:03 +0100

    Add files via upload

--- src/_11_12_2023/task.java
+++ src/_11_12_2023/task.java
@@ -63,5 +63,77 @@ public class task {
 
 // Set<String> findAllValues(Set<Map<Integer, String>> setOfMaps) {
 //}Вывести все строки, которые являются значениями в любой из мап.
+    public static Set<String> findAllValues(Set<Map<Integer, String>> setOfMaps){
+        Set<String> set = new HashSet<>();
+        for (Map<Integer, String> integerStringMap : setOfMaps) {
+            for (Map.Entry<Integer, String> entry : integerStringMap.entrySet()) {
+                set.add(entry.getValue());
+            }
+        }
+        return set;
+    }
 
+//int sumAllKeys(Set<Map<Integer, String>> setOfMaps) {
+//}Найти сумму всех ключей всех мап.
+
+    public static int sumAllKeys(Set<Map<Integer, String>> setOfMaps){
+        int sum = 0;
+        for (Map<Integer, String> element : setOfMaps ) {
+            for (int key : element.keySet() ) {
+                sum += key;
+            }
+        }
+        return sum;
+    }
+
+//    boolean containsValue(Set<Map<Integer, String>> setOfMaps, String value) {
+//}Определить, содержится ли заданная строка как значение хотя бы в одной из мапы.
+
+    public static boolean containsValue(Set<Map<Integer, String>> setOfMaps, String value){
+        for (Map<Integer, String> element : setOfMaps) {
+            for (String values : element.values()) {
+                if(value.equals(values)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+//    Set<String> findAllStringsWithChar(Set<Map<Character, Set<String>>> complexSet, char c) {
+//}Найти все строки, которые начинаются с заданного символа во всех вложенных сетах.
+
+    public static Set<String> findAllStringsWithChar(Set<Map<Character, Set<String>>> complexSet, char c){
+        Set<String> set = new HashSet<>();
+        for (Map<Character, Set<String>> element : complexSet) {
+            for (Set<String> stringSet : element.values()) {
+                for (String str : stringSet) {
+                    if(str.charAt(0) == c){
+                        set.add(str);
+                    }
+                }
+            }
+        }
+        return set;
+    }
+
+//    Map<Character, Integer> countStringOccurrences(Set<Map<Character, Set<String>>> complexSet) {
+//}Создать мапу, где ключами являются символы, а значениями - количество раз, которое
+//строки начинающиеся на этот символ встречаются во всех вложенных сетах.
+
+    public static Map<Character, Integer> countStringOccurrences(Set<Map<Character, Set<String>>> complexSet) {
+        Map<Character, Integer> newMap = new HashMap<>();
+        for (Map<Character, Set<String>> element : complexSet) {
+            for (Map.Entry<Character, Set<String>> entry : element.entrySet()) {
+                int count = 0;
+                for (String value : entry.getValue()) {
+                    if(value.charAt(0) == entry.getKey()){
+                        count ++;
+                    }
+                }
+                newMap.put(entry.getKey(), count);
+            }
+        }
+        return newMap;
+    }
 }
