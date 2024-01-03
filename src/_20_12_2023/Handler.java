60377b4d8287da6e7cbd3385b42b9e859584428d
Savelii.Apanasenko <2005savaapanasenko@gmail.com>
2024-01-03 23:16:47 +0100

    Add files via upload

--- src/_20_12_2023/Handler.java
+++ src/_20_12_2023/Handler.java
@@ -8,21 +8,22 @@ import static _20_12_2023.Generator.generator;
 
 public class Handler {
     public static void main(String[] args) {
-        Generator.generator(Adult.class);
-        System.out.println("----------------------------------------------------------------");
-        Generator.generator(Teenager.class);
-        System.out.println("----------------------------------------------------------------");
-        Generator.generator(Pupil.class);
-        System.out.println("----------------------------------------------------------------");
-        Set<Team<Adult>> teamAdult = generator(Adult.class);
-        Play.play(teamAdult);
-        System.out.println("----------------------------------------------------------------");
-        Set<Team<Teenager>> teamTeenager = generator(Teenager.class);
-        Play.play(teamTeenager);
-        System.out.println("----------------------------------------------------------------");
-        Set<Team<Pupil>> teamPupil = generator(Pupil.class);
-        Play.play(teamPupil);
-        System.out.println("_------------------------------");
+          Set<Team<Adult>> teamAdult = Generator.generator(Adult.class);
+//          System.out.println("----------------------------------------------------------------");
+//          Set<Team<Teenager>> teamTeenager = Generator.generator(Teenager.class);
+//          System.out.println("----------------------------------------------------------------");
+//          Set<Team<Pupil>> teamPupil = Generator.generator(Pupil.class);
+          System.out.println("----------------------------------------------------------------");
+          Map<Team<Adult>, Double> team1 = Play.play(teamAdult);
+          System.out.println("----------------------------------------------------------------");
+//          Map<Team<Teenager>, Double> team2 = Play.play(teamTeenager);
+//          System.out.println("----------------------------------------------------------------");
+//          Map<Team<Pupil>, Double> team3 = Play.play(teamPupil);
+//          System.out.println("_----------------------------------------------------------------");
+          Play.semiFinal(team1);
+//          Play.semiFinal(team2);
+//          Play.semiFinal(team3);
+
     }
 }
 
--- src/_20_12_2023/Play.java
+++ src/_20_12_2023/Play.java
@@ -2,15 +2,15 @@ package _20_12_2023;
 
 import java.util.*;
 
-public class Play extends Participant{
+public class Play extends Participant {
 
     public Play(String name, int age) {
         super(name, age);
     }
 
-    public static <T extends Participant>  Map<Team<T>, Double> play(Set<Team<T>> teams) {
+    public static <T extends Participant> Map<Team<T>, Double> play(Set<Team<T>> teams) {
         Map<Team<T>, Double> map = new HashMap<>();
-        for (Team<T> team : teams){
+        for (Team<T> team : teams) {
             map.put(team, 0.0);
         }
         for (Team<T> team1 : map.keySet()) {
@@ -40,5 +40,31 @@ public class Play extends Participant{
         Random random = new Random();
         int randomIndex = random.nextInt(values.length);
         return values[randomIndex];
+    }
+    public static <T extends Participant> void semiFinal(Map<Team<T>, Double> map) {
+        Map<Team<T>, Double> team = new HashMap<>(map);
+        Map<Team<T>, Double> teamSemiFinal = new HashMap<>();
+        for (Map.Entry<Team<T>, Double> entry : map.entrySet()) {
+            if (entry.getValue() == 0.0) {
+                team.remove(entry.getKey(), entry.getValue());
+            }
+            if (entry.getValue() == 0.5) {
+                teamSemiFinal.put(entry.getKey(), entry.getValue());
+            }
+        }
+        boolean isFirstTeam = true;
+        for (Team<T> team1 : teamSemiFinal.keySet()) {
+            team.put(team1, isFirstTeam ? 1.0 : 0.0);
+            isFirstTeam = !isFirstTeam;
+        }
+        Map<Team<T>, Double> finalTeam = new HashMap<>();
+        for (Map.Entry<Team<T>, Double> entry : team.entrySet()) {
+            if (entry.getValue() != 0.0) {
+                finalTeam.put(entry.getKey(), entry.getValue());
+            }
+        }
+        for (Map.Entry<Team<T>, Double> entry : finalTeam.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
