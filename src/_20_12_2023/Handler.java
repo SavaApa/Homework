2d4ba0bb0e065b17b5d436fa3cfab8b64ca8e012
Savelii.Apanasenko <2005savaapanasenko@gmail.com>
2024-01-08 23:10:25 +0100

    Add files via upload

--- src/_20_12_2023/Handler.java
+++ src/_20_12_2023/Handler.java
@@ -8,28 +8,17 @@ import static _20_12_2023.Generator.generator;
 
 public class Handler {
     public static void main(String[] args) {
-          Set<Team<Adult>> teamAdult = Generator.generator(Adult.class);
-          System.out.println("----------------------------------------------------------------");
-//          Set<Team<Teenager>> teamTeenager = Generator.generator(Teenager.class);
-//          System.out.println("----------------------------------------------------------------");
-//          Set<Team<Pupil>> teamPupil = Generator.generator(Pupil.class);
-//          System.out.println("----------------------------------------------------------------");
-          Map<Team<Adult>, Double> team1 = Play.play(teamAdult);
-          System.out.println("----------------------------------------------------------------");
-//          Map<Team<Teenager>, Double> team2 = Play.play(teamTeenager);
-//          System.out.println("----------------------------------------------------------------");
-//          Map<Team<Pupil>, Double> team3 = Play.play(teamPupil);
-//          System.out.println("-----------------------------------------------------------------");
-          Map<Team<Adult>, Double> semiFinalAdult = Play.semiFinal(team1);
-//          System.out.println("-----------------------------------------------------------------");
-//          Play.semiFinal(team2);
-//          System.out.println("-----------------------------------------------------------------");
-//          Play.semiFinal(team3);
-        System.out.println("------------------------------------------------------------------------");
-        Play.finalPlay(semiFinalAdult);
-
-
-
+        Set<Team<Adult>> teamAdult = Generator.generator(Adult.class);
+        System.out.println("----------------------------------------------------------------");
+        Play.play(teamAdult);
+        System.out.println("----------------------------------------------------------------");
+        Set<Team<Teenager>> teamTeenager = Generator.generator(Teenager.class);
+        System.out.println("----------------------------------------------------------------");
+        Play.play(teamTeenager);
+        System.out.println("----------------------------------------------------------------");
+        Set<Team<Pupil>> teamPupil = Generator.generator(Pupil.class);
+        System.out.println("----------------------------------------------------------------");
+        Play.play(teamPupil);
     }
 }
 
--- src/_20_12_2023/Play.java
+++ src/_20_12_2023/Play.java
@@ -8,32 +8,49 @@ public class Play extends Participant {
         super(name, age);
     }
 
-    public static <T extends Participant> Map<Team<T>, Double> play(Set<Team<T>> teams) {
-        Map<Team<T>, Double> map = new HashMap<>();
+    public static <T extends Participant> void play(Set<Team<T>> teams) {
+        Map<Team<T>, Double> results = new HashMap<>();
+
         for (Team<T> team : teams) {
-            map.put(team, 0.0);
+            results.put(team, 0.0);
         }
-        for (Team<T> team1 : map.keySet()) {
-            for (Team<T> team2 : map.keySet()) {
-                double result = getRandomValue();
-                if (team1 != team2) {
-                    if (result == 1) {
-                        map.put(team1, 1.0);
-                        map.put(team2, 0.0);
-                    } else if (result == 0.5) {
-                        map.put(team1, 0.5);
-                        map.put(team2, 0.5);
-                    } else {
-                        map.put(team1, 0.0);
-                        map.put(team2, 1.0);
-                    }
-                }
+
+        List<Team<T>> teamsList = new ArrayList<>(teams);
+        for (int i = 0; i < teamsList.size(); i++) {
+            Team<T> team1 = teamsList.get(i);
+            for (int j = i + 1; j < teamsList.size(); j++) {
+                Team<T> team2 = teamsList.get(j);
+                playMatch(team1, team2, results);
             }
         }
-        for (Map.Entry<Team<T>, Double> entry : map.entrySet()) {
+
+        List<Map.Entry<Team<T>, Double>> sortedResults = new ArrayList<>(results.entrySet());
+        sortedResults.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        System.out.println("Final Standings:");
+        for (Map.Entry<Team<T>, Double> entry : sortedResults) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-        return map;
+    }
+
+    private static <T extends Participant> void playMatch(Team<T> team1, Team<T> team2, Map<Team<T>, Double> results) {
+        double result = getRandomValue();
+        double pointsTeam1 = results.getOrDefault(team1, 0.0);
+        double pointsTeam2 = results.getOrDefault(team2, 0.0);
+
+        if (result == 1) {
+            pointsTeam1 += 1.0;
+            pointsTeam2 += 0.0;
+        } else if (result == 0.5) {
+            pointsTeam1 += 0.5;
+            pointsTeam2 += 0.5;
+        } else {
+            pointsTeam1 += 0.0;
+            pointsTeam2 += 1.0;
+        }
+
+        results.put(team1, pointsTeam1);
+        results.put(team2, pointsTeam2);
     }
 
     private static double getRandomValue() {
@@ -41,75 +58,6 @@ public class Play extends Participant {
         Random random = new Random();
         int randomIndex = random.nextInt(values.length);
         return values[randomIndex];
-    }
-
-    public static <T extends Participant> Map<Team<T>, Double> semiFinal(Map<Team<T>, Double> map) {
-        Map<Team<T>, Double> team = new HashMap<>(map);
-        Map<Team<T>, Double> teamSemiFinal = new HashMap<>();
-        for (Map.Entry<Team<T>, Double> entry : map.entrySet()) {
-            if (entry.getValue() == 0.0) {
-                team.remove(entry.getKey(), entry.getValue());
-            }
-            if (entry.getValue() == 0.5) {
-                teamSemiFinal.put(entry.getKey(), entry.getValue());
-            }
-        }
-        boolean isFirstTeam = true;
-        for (Team<T> team1 : teamSemiFinal.keySet()) {
-            team.put(team1, isFirstTeam ? 1.0 : 0.0);
-            isFirstTeam = !isFirstTeam;
-        }
-        Map<Team<T>, Double> finalTeam = new HashMap<>();
-        for (Map.Entry<Team<T>, Double> entry : team.entrySet()) {
-            if (entry.getValue() != 0.0) {
-                finalTeam.put(entry.getKey(), entry.getValue());
-            }
-        }
-        for (Map.Entry<Team<T>, Double> entry : finalTeam.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-        return finalTeam;
-    }
-
-    public static <T extends Participant> void finalPlay(Map<Team<T>, Double> teams) {
-        List<Team<T>> teamList = new ArrayList<>(teams.keySet());
-
-        while (teamList.size() > 1) {
-            if (teamList.size() % 2 != 0) {
-                Team<T> byeTeam = teamList.remove(teamList.size() - 1);
-                teams.put(byeTeam, teams.getOrDefault(byeTeam, 0.0) + 1.0);
-            }
-            List<Team<T>> winners = new ArrayList<>();
-
-            for (int i = 0; i < teamList.size(); i += 2) {
-                if (i + 1 < teamList.size()) {
-                    Team<T> team1 = teamList.get(i);
-                    Team<T> team2 = teamList.get(i + 1);
-
-                    double pointsTeam1 = teams.getOrDefault(team1, 0.0);
-                    double pointsTeam2 = teams.getOrDefault(team2, 0.0);
-
-                    // Играем обычный раунд
-                    boolean team1Wins = pointsTeam1 > pointsTeam2;
-                    pointsTeam1 += team1Wins ? 1.0 : 0.0;
-                    pointsTeam2 += team1Wins ? 0.0 : 1.0;
-
-                    teams.put(team1, pointsTeam1);
-                    teams.put(team2, pointsTeam2);
-                    winners.add(team1Wins ? team1 : team2);
-                }
-            }
-
-            teamList = winners;
-        }
-
-        List<Map.Entry<Team<T>, Double>> entries = new ArrayList<>(teams.entrySet());
-        entries.sort(Map.Entry.<Team<T>, Double>comparingByValue().reversed());
-
-        // Выводим все команды
-        for (Map.Entry<Team<T>, Double> entry : entries) {
-            System.out.println(entry.getKey() + ": " + entry.getValue() + " points");
-        }
     }
 }
 
