3532a54c413ab34bfbed7245e7aefe4649001408
Savelii.Apanasenko <2005savaapanasenko@gmail.com>
2024-01-25 19:12:04 +0100

    Add files via upload

--- src/_22_01_2024/Task4.java
+++ src/_22_01_2024/Task4.java
@@ -6,32 +6,28 @@ import java.util.List;
 public class Task4 {
     public static void main(String[] args) {
 //        Найдите стримы с минимальным рейтингом и выведите их в новый файл.
-        try {
-            try (BufferedReader reader = new BufferedReader(new FileReader("C:\\projects\\homework\\src\\_22_01_2024\\f.txt"));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter("result_task.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\projects\\homework\\src\\_22_01_2024\\f.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("result_task.txt"))) {
 
-                List<String> lines = reader.lines().toList();
-
-
-                double minRating = lines.stream()
-                        .map(line -> Double.parseDouble(line.split(", ")[1]))
-                        .min(Double::compare)
-                        .orElse(0.0);
+            List<String> lines = reader.lines().toList();
 
 
-                List<String> minRatingStreams = lines.stream()
-                        .filter(line -> Double.parseDouble(line.split(", ")[1]) == minRating)
-                        .toList();
-
-                for (String s : minRatingStreams) {
-                    writer.write(s);
-                    writer.newLine();
-                }
+            double minRating = lines.stream()
+                    .mapToDouble(line -> Double.parseDouble(line.split(", ")[1]))
+                    .min()
+                    .orElse(0.0);
 
 
-            } catch (IOException e) {
-                e.printStackTrace();
+            List<String> minRatingStreams = lines.stream()
+                    .filter(line -> Double.parseDouble(line.split(", ")[1]) == minRating)
+                    .toList();
+
+            for (String s : minRatingStreams) {
+                writer.write(s);
+                writer.newLine();
             }
+
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
