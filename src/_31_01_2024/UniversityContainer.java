7cb66b73417fe31841ebc037fbb49ebd1b2a8929
Savelii.Apanasenko <2005savaapanasenko@gmail.com>
2024-01-30 22:09:40 +0100

    Add files via upload

---
+++ src/_31_01_2024/UniversityContainer.java
@@ -0,0 +1,13 @@
+package _31_01_2024;
+
+public class UniversityContainer {
+    private University university;
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+}
